2d60049afc7d5062a243b8dd862428346472394d
Evan White <evanwht1@gmail.com>
2020-05-04 09:15:18 -0500

    jsx props

--- public/app.jsx
+++ public/app.jsx
@@ -1,14 +1,26 @@
 var Greeter = React.createClass({
+    getDefaultProps: function() {
+        return {
+            name: 'React',
+            message: 'Default jsx component'
+        }
+    },
     render: function() {
+        var name = this.props.name;
+        var message = this.props.message
         return (
             <div>
-                <h1>Hello React!</h1>
+                <h1>Hello {name}!</h1>
+                <p>{message + '!'}</p>
             </div>
         );
     }
 });
 
+var myName = 'Evan';
+var myMessage = 'New variable message';
+
 ReactDOM.render(
-    <Greeter/>,
+    <Greeter name={myName} message={myMessage}/>,
     document.getElementById('app')
 );
